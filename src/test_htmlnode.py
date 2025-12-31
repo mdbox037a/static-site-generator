782bfcaf5b02923d5d3c7d955ac47ccc2af9aa03
matt box <mdbox037a@protonmail.com>
2025-12-31 14:04:01 -0500

    combine html node and leaf tests into single file and remove test_leafnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -17,6 +17,27 @@ class TestHTMLNode(unittest.TestCase):
     def test_props(self):
         node = HTMLNode(None, None, None, {"key1": "value1", "key2": "value2"})
         self.assertEqual(node.props_to_html(), ' key1="value1" key2="value2"')
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.example.com"})
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.example.com">Click me!</a>'
+        )
+
+    def test_leaf_to_html_aa(self):
+        node = LeafNode(
+            "a", "Click me!", {"href": "https://www.example.com", "target": "_blank"}
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.example.com" target="_blank">Click me!</a>',
+        )
 
 
 if __name__ == "__main__":
