79091e9627d61c63bccba48ef96a7ff8c5447344
matt box <mdbox037a@protonmail.com>
2026-01-01 16:20:57 -0500

    add text_to_html_node() function to textnode.py; adjust LeafNode class tag type hint to accept None default

--- src/textnode.py
+++ src/textnode.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 from enum import Enum
+from htmlnode import HTMLNode, LeafNode
 
 
 class TextType(Enum):
-    PLAIN = "plain: text"
+    TEXT = "plain: text"
     BOLD = "bold: **Bold text**"
     ITALIC = "italic: _Italic text_"
     CODE = "code: `code text`"
@@ -29,3 +30,26 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    """Return a LeafNode object based on params of text_node"""
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(tag=None, value=text_node.text, props=None)
+        case TextType.BOLD:
+            return LeafNode(tag="b", value=text_node.text, props=None)
+        case TextType.ITALIC:
+            return LeafNode(tag="i", value=text_node.text, props=None)
+        case TextType.CODE:
+            return LeafNode(tag="code", value=text_node.text, props=None)
+        case TextType.LINK:
+            return LeafNode(
+                tag="a", value=text_node.text, props={"href": text_node.url}
+            )
+        case TextType.IMAGE:
+            return LeafNode(
+                tag="img", value="", props={"src": text_node.url, "alt": text_node.text}
+            )
+        case _:
+            raise Exception(f"Invalid TextType for node {text_node}")
