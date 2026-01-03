adaac1dbd9fe2d666bbf6440a418ea4ec38237a4
matt box <mdbox037a@protonmail.com>
2026-01-02 19:04:13 -0500

    add further unit tests for error checking and mixed texttype lists

--- src/test_processmarkdown.py
+++ src/test_processmarkdown.py
@@ -27,6 +27,37 @@ class TestProcessMarkdown(unittest.TestCase):
             [TextNode("test code block", TextType.CODE)],
         )
 
+    def test_typical_mix(self):
+        node1 = TextNode(
+            "Here is a first sample `code goes here` sample continues", TextType.TEXT
+        )
+        node2 = TextNode(
+            "Here is a second sample **bold goes here** sample continues", TextType.TEXT
+        )
+        old_nodes = [node1, node2]
+        self.assertEqual(
+            split_nodes_delimiter(old_nodes, "`", TextType.CODE),
+            [
+                TextNode("Here is a first sample ", TextType.TEXT),
+                TextNode("code goes here", TextType.CODE),
+                TextNode(" sample continues", TextType.TEXT),
+                TextNode(
+                    "Here is a second sample **bold goes here** sample continues",
+                    TextType.TEXT,
+                ),
+            ],
+        )
+
+    def test_missing_delimiter(self):
+        node = TextNode("Testing missing _italic delimiter", TextType.TEXT)
+        old_nodes = [node]
+        with self.assertRaises(ValueError) as context_manager:
+            split_nodes_delimiter(old_nodes, "_", TextType.ITALIC)
+        self.assertEqual(
+            str(context_manager.exception),
+            "invalid markdown, formatted section not closed",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
