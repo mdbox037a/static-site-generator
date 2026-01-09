fb0c5b10908c91adc66e308153fbf8d6c27f5b00
matt box <mdbox037a@protonmail.com>
2026-01-09 06:59:39 -0500

    complete paragraph case in markdown_to_html_node(); begin work to break text-to-child-list process there into helper function that can be used for other BlockType cases

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,7 @@
 from enum import Enum
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, ParentNode
+from processmarkdown import TextNode, TextType, text_to_textnodes
+from textnode import text_node_to_html_node
 
 
 class BlockType(Enum):
@@ -58,6 +60,18 @@ def markdown_to_html_node(markdown: str) -> HTMLNode:
     all HTMLNode children representing its nested elements
     """
     blocks = markdown_to_blocks(markdown)
+    children = []
     for block in blocks:
-        type_string = block_to_block_type(block)
-        pass  # progress marker
+        btype = block_to_block_type(block)
+        match btype:
+            case BlockType.PARAGRAPH:
+                child_hnodes = []
+                tnodes = text_to_textnodes(block)
+                for tnode in tnodes:
+                    child_hnodes.append(text_node_to_html_node(tnode))
+                parent_hnode = ParentNode("p", child_hnodes)
+
+
+def text_to_children(text: str) -> list[HTMLNode]:
+    """Accept a string and return a list of HTMLNodes that represent inline markdown children"""
+    pass  # progress marker
