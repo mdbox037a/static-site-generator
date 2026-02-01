f5c6634aa95d4c963185f2e72ea5ea592420daf5
matt box <mdbox037a@protonmail.com>
2026-02-01 16:49:56 -0500

    update main() to call index.html generator function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from textnode import TextNode, TextType
 from copystatic import copy_static
+from gencontent import generate_page
 
 
 def main() -> None:
     copy_static()
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
